52d9ff86bf6703069b127774f7060443683c7d77
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2024-01-16 16:22:37 +0900

    calibrationのリファクタリング

--- news_recommender_metrics/RADio/calibration.py
+++ news_recommender_metrics/RADio/calibration.py
@@ -1,12 +1,6 @@
-from typing import Any, Dict, List
+from typing import Any
 
-from pydantic.dataclasses import dataclass
-
-from news_recommender_metrics.RADio.dart_metrics_abstract import DartMetricsAbstract
-from news_recommender_metrics.RADio.divergence_metrics import (
-    DivergenceMetricAbstract,
-    JSDivergence,
-)
+from news_recommender_metrics.RADio.divergence_metrics import JSDivergence
 from news_recommender_metrics.utils.probability_mass_function.probability_mass_function import (
     ProbabilityMassFunction,
 )
@@ -15,53 +9,29 @@ from news_recommender_metrics.utils.probability_mass_function.rank_aware_probabi
 )
 
 
-@dataclass
-class Calibration(DartMetricsAbstract):
-    value: float
-    P_dist: Dict[Any, float]
-    Q_dist: Dict[Any, float]
-
-    @classmethod
-    def calc(
-        cls,
-        reading_history: List[Any],
-        recommendations: List[Any],
+class Calibration:
+    def __init__(
+        self,
         is_rank_aware: bool = True,
         rank_weight_method: str = "MMR",
-    ) -> "Calibration":
-        """_summary_
+    ) -> None:
+        self.is_rank_aware = is_rank_aware
+        self.rank_weight_method = rank_weight_method
 
-        Parameters
-        ----------
-        reading_history : List[Any]
-            the user's reading history of news. element is an item attribute for calculate Calibration.(ex. news category label)
-        recommendations : List[Any]
-            recommendation result for the user. it's assumed being sorted by recommendation ranking.
-        is_rank_aware : bool
-            if True, calculate Calibration with rank-aware distribution of recommendations.
-            Else, calculate without considering ranking.
-        rank_weight_method: str = "MMR"
-            the method for weighting of the rank.("MMR" or "nDCG")
-        Returns
-        -------
-        Calibration
-            Calibration of the item attribute.
-        """
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.forward(*args, **kwds)
 
+    def forward(
+        self,
+        reading_history: list[str],
+        recommendations: list[str],
+    ) -> float:
         P_dist = ProbabilityMassFunction.from_list(reading_history)
 
         Q_dist = (
-            RankAwareProbabilityMassFunction.from_ranking(
-                recommendations, rank_weight_method
-            )
-            if is_rank_aware
+            RankAwareProbabilityMassFunction.from_ranking(recommendations, self.rank_weight_method)
+            if self.is_rank_aware
             else ProbabilityMassFunction.from_list(recommendations)
         )
         calculator = JSDivergence()
-        value = calculator.calc(P_dist.pmf, Q_dist.pmf)
-
-        return Calibration(
-            value=value,
-            P_dist=P_dist.pmf,
-            Q_dist=Q_dist.pmf,
-        )
+        return calculator.calc(P_dist.pmf, Q_dist.pmf)
--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -4,28 +4,17 @@ from news_recommender_metrics.RADio.calibration import Calibration
 
 
 def test_calibration() -> None:
-    reading_history = ["b", "a", "a"]
-    recommendations = ["a", "b", "b"]
-
-    calbration_actual = Calibration.calc(
-        reading_history,
-        recommendations,
-        True,
-        "MMR",
+    # Arrange
+    reading_history_of_user_A = ["bussiness", "technology", "technology"]
+    recommended_topics_of_user_A = ["technology", "bussiness", "bussiness"]
+    sut = Calibration(
+        is_rank_aware=True,
+        rank_weight_method="MMR",
     )
 
-    P_dist_expected = {"a": 0.666, "b": 0.333}
-    Q_dist_expected = {
-        "a": 0.545454545,
-        "b": 0.454545454,
-    }
+    # Act
+    calbration_actual = sut(reading_history_of_user_A, recommended_topics_of_user_A)
+
+    # Assert
     value_expected = 0.011127  # JS Divergence of P_dist & Q_dist
-    assert [
-        math.isclose(prob_mass, P_dist_expected.get(x, 0))
-        for x, prob_mass in calbration_actual.P_dist.items()
-    ]
-    assert [
-        math.isclose(prob_mass, Q_dist_expected.get(x, 0))
-        for x, prob_mass in calbration_actual.Q_dist.items()
-    ]
-    assert math.isclose(round(calbration_actual.value, 6), value_expected)
+    assert math.isclose(calbration_actual, value_expected, rel_tol=1e-2)
